6e15828baf855e7152bc90c18a85b5a98a6539f1
Vitaliy <k.vitaliy1994@gmail.com>
2019-07-07 21:12:25 +0300

    copypasted some code from tutorial work-in-process

---
+++ src/main/java/vitaliy94/bookshoptest/config/JwtAuthEntryPoint.java
@@ -0,0 +1,27 @@
+package vitaliy94.bookshoptest.config;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class JwtAuthEntryPoint implements AuthenticationEntryPoint
+{
+    @Override
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException e)
+            throws IOException, ServletException {
+
+//        log.error("Unauthorized error. Message - {}", e.getMessage());
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Error -> Unauthorized");
+    }
+}
---
+++ src/main/java/vitaliy94/bookshoptest/config/JwtProvider.java
@@ -0,0 +1,58 @@
+package vitaliy94.bookshoptest.config;
+
+import io.jsonwebtoken.*;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+@Slf4j
+public class JwtProvider
+{
+    private String jwtSecret = "jwtsecret";
+    private int jwtExpiration = 86400;
+
+    public String generateJwtToken(Authentication authentication) {
+
+        UserPrinciple userPrincipal = (UserPrinciple) authentication.getPrincipal();
+
+        return Jwts.builder()
+                .setSubject((userPrincipal.getUsername()))
+                .setIssuedAt(new Date())
+                .setExpiration(new Date((new Date()).getTime() + jwtExpiration*1000))
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .compact();
+    }
+
+    public boolean validateJwtToken(String authToken) {
+        try {
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(authToken);
+            return true;
+        } catch (SignatureException e) {
+//            log.error("Invalid JWT signature -> Message: {} ", e);
+        } catch (MalformedJwtException e) {
+//            log.error("Invalid JWT token -> Message: {}", e);
+        } catch (ExpiredJwtException e) {
+//            log.error("Expired JWT token -> Message: {}", e);
+        } catch (UnsupportedJwtException e) {
+//            log.error("Unsupported JWT token -> Message: {}", e);
+        } catch (IllegalArgumentException e) {
+//            log.error("JWT claims string is empty -> Message: {}", e);
+        }
+
+        return false;
+    }
+
+    public String getUserNameFromJwtToken(String token) {
+        return Jwts.parser()
+                .setSigningKey(jwtSecret)
+                .parseClaimsJws(token)
+                .getBody().getSubject();
+    }
+
+}
